6f62e1b45ea262007768d711e84243d390c71dc8
humbejeff2116 <humbejeff2116@gmail.com>
2021-03-06 22:16:39 +0000

    implemented a useEffect hook in suspenseLoader

--- src/components/suspenseLoader/SuspLoader.js
+++ src/components/suspenseLoader/SuspLoader.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React,{ useEffect } from 'react';
 import Template from '../template/Template';
 import {SkeletonLoader} from '../Loader/loader';
 import './SuspLoader.css'
@@ -7,6 +7,11 @@ import './SuspLoader.css'
 
 // app fallback suspense loader 
 export default function FallbackLoader(props){
+    useEffect(()=>{
+        window.scrollTo(0,0)
+
+
+    },[])
     return(
         <Template >
             <SkeletonLoader/>
